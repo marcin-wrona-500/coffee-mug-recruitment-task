2dd89abec753dc56215cfcb57131e0602ac3079b
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-20 22:19:11 +0100

    middleware/yupValidator: Support the "params" property

--- src/middleware/yupValidator/types.d.ts
+++ src/middleware/yupValidator/types.d.ts
@@ -2,5 +2,5 @@ import { Request } from 'express';
 import { AnySchema } from 'yup';
 
 // picking the keys from "Request" ensures that the keys defined here actually exist on the Request type
-export type ValidatorSchemaKeys = keyof Pick<Request, 'query' | 'body'>;
+export type ValidatorSchemaKeys = keyof Pick<Request, 'query' | 'body' | 'params'>;
 export type ValidatorSchemas = Partial<Record<ValidatorSchemaKeys, AnySchema>>;
