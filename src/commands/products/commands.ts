bbda19589f483692220fb807f2a3fab390f6486c
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-20 20:36:18 +0100

    Implement the "POST /products" endpoint and the "create a Product" command

--- src/commands/products/commands.ts
+++ src/commands/products/commands.ts
@@ -2,3 +2,14 @@ import yup from 'yup';
 
 export const getProductsCommand = yup.mixed().oneOf([undefined]);
 export type GetProductsCommand = yup.InferType<typeof getProductsCommand>;
+
+export const createProductCommand = yup
+	.object({
+		name: yup.string().required().max(50),
+		description: yup.string().required().max(50),
+		price: yup.number().required().positive(),
+		stock: yup.number().required().min(0),
+	})
+	.required()
+	.exact();
+export type CreateProductCommand = yup.InferType<typeof createProductCommand>;
