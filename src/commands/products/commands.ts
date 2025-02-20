647a9d5d032959785ef9b121951d7739a028d6a2
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-20 20:38:51 +0100

    commands/products: Fix "yup" import
    Not quite sure why, but I swear that "regular" import syntax
    worked fine a few commits ago, despite Yup requiring this syntax
    in previous projects I worked on...

--- src/commands/products/commands.ts
+++ src/commands/products/commands.ts
@@ -1,4 +1,4 @@
-import yup from 'yup';
+import * as yup from 'yup';
 
 export const getProductsCommand = yup.mixed().oneOf([undefined]);
 export type GetProductsCommand = yup.InferType<typeof getProductsCommand>;
