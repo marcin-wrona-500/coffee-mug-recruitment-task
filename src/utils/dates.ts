065377a7c49637ed6036734726180137cd0267fb
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-22 14:19:47 +0100

    middleware/discounts: Implement Black Friday and bank holiday discounts

---
+++ src/utils/dates.ts
@@ -0,0 +1,5 @@
+// checks for *date* equality - without the time (hour)
+export const areDatesEqual = (a: Date, b: Date) =>
+	a.getFullYear() === b.getFullYear() &&
+	a.getMonth() === b.getMonth() &&
+	a.getDate() === b.getDate();
