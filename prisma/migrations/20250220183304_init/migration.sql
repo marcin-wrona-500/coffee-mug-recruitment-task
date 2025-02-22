cbfec94fb9750286d08772eae067be3b8415ebba
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-22 14:31:51 +0100

    prisma: Add missing "UNSIGNED" attribute on columns in the Product table
    Since prisma does not store this important attribute in the schema, the
    resulting migration DDL needs to be manually modified after running
    the "migrate dev" command.

--- prisma/migrations/20250220183304_init/migration.sql
+++ prisma/migrations/20250220183304_init/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE `Product` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL,
     `description` VARCHAR(50) NOT NULL,
-    `price` DECIMAL(7, 2) NOT NULL,
-    `stock` DECIMAL(10, 0) NOT NULL,
+    `price` DECIMAL(7, 2) UNSIGNED NOT NULL,
+    `stock` DECIMAL(10, 0) UNSIGNED NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
