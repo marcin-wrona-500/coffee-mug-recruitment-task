379873a1996134ce06c2f01dfbb697065f440fae
Marcin Wrona <marcinwrona500@gmail.com>
2025-02-22 14:55:18 +0100

    middleware/yupValidator: Use the asynchronous "validate" method
    This allows the definition of asynchronous tests on the schemas

--- src/middleware/yupValidator/validator.ts
+++ src/middleware/yupValidator/validator.ts
@@ -1,16 +1,16 @@
 import { RequestHandler } from 'express';
+import expressAsyncHandler from 'express-async-handler';
 
 import { ValidatorSchemas } from './types';
 
-const validator =
-	(schemas: ValidatorSchemas): RequestHandler =>
-	(req, _res, next) => {
+const validator = (schemas: ValidatorSchemas): RequestHandler =>
+	expressAsyncHandler(async (req, _res, next) => {
 		if (!req.parsed) req.parsed = {};
 		for (const [_key, schema] of Object.entries(schemas)) {
 			const key = _key as keyof ValidatorSchemas;
-			req.parsed[key] = schema.validateSync(req[key]);
+			req.parsed[key] = await schema.validate(req[key]);
 		}
 		next();
-	};
+	});
 
 export default validator;
